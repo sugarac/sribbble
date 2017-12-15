4b4ae8750a60604d4bdf40d16134fcc79c7cb5b1
sugarac <uscwangxu@gmail.com>
2017-12-14 19:28:54 -0800

    complete shot list with fake data

--- app/src/main/java/com/sugarac/sribbble/view/MainActivity.java
+++ app/src/main/java/com/sugarac/sribbble/view/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.sugarac.sribbble.R;
+import com.sugarac.sribbble.view.shot_list.ShotListFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) { //for not adding fragment twice
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, ShotListFragment.newInstance())
+                    .commit();
+        }
     }
 }
